39a6a2ef5f1383301432640acaa33a406018b77b
Alessandro <vyseclown@gmail.com>
2020-01-14 22:44:05 -0300

    Terminado text

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Scaffold(
+        body: HomeWidget(),
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300,
+        height: 50,
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: Text(
+          "Meu primeiro texto",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+      ),
     );
   }
 }
